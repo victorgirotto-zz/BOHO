953d25fe0eb3fafbb5ddc5eb96d4030800634bf3
Victor Girotto <victor.girotto@outlook.com>
2017-05-07 16:46:52 -0600

    Working pay button

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(function(){
     
+    $('#recurring').click(function(e){
+        var checked = $(this).is(':checked');
+        $('.recurring').prop('disabled', !checked);
+        $('.donation').prop('disabled', checked);
+    });
+
+    // Keep a3 input in sync with selection
+    $('[name=amount]').click(function(e){
+        var amount = $(this).val();
+        $('#a3').val(amount);
+    });
+
 });
